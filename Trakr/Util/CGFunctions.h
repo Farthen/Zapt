b11176f7856905aa8ac804c45bed8916c4c6b562
farthen <wilkefi@googlemail.com>
2013-03-11 02:02:03 +0100

    Some CG convenience functions

---
+++ Trakr/Util/CGFunctions.h
@@ -0,0 +1,14 @@
+//
+//  CGFunctions.h
+//  Trakr
+//
+//  Created by Finn Wilke on 11.03.13.
+//  Copyright (c) 2013 Finn Wilke. All rights reserved.
+//
+
+#ifndef Trakr_CGFunctions_h
+#define Trakr_CGFunctions_h
+
+
+
+#endif
