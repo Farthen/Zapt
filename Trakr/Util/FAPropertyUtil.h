1c923e51fa8a4b528f733026f71b4482a36edeca
farthen <wilkefi@googlemail.com>
2013-03-15 00:16:39 +0100

    include util as symlinks

--- Trakr/Util/FAPropertyUtil.h
+++ Trakr/Util/FAPropertyUtil.h
@@ -1,15 +1,1 @@
-//
-//  FAPropertyUtil.h
-//  Trakr
-//
-//  Created by Finn Wilke on 11.10.12.
-//  Copyright (c) 2012 Finn Wilke. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-#import "FAPropertyInfo.h"
-
-@interface FAPropertyUtil : NSObject
-
-+ (NSDictionary *)propertyInfoForClass:(Class)class;
-@end
+../../../util/FAPropertyUtil.h
--- Trakr/Util/UIFunctions.h
+++ Trakr/Util/UIFunctions.h
@@ -1,13 +1,1 @@
-//
-//  UIFunctions.h
-//  Trakr
-//
-//  Created by Finn Wilke on 11.03.13.
-//  Copyright (c) 2013 Finn Wilke. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-UIEdgeInsets UIEdgeInsetsAdd(UIEdgeInsets a, UIEdgeInsets b);
-UIEdgeInsets UIEdgeInsetsSubtract(UIEdgeInsets a, UIEdgeInsets b);
+../../../util/UIFunctions.h
