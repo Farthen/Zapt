062c46829d51e9756bbfa8b57024b61346574c20
farthen <monkey@farthen.de>
2013-08-27 19:50:06 +0200

    Commit updated Pods files

--- Pods/BuildHeaders/LRResty/NSRunLoop+Additions.h
+++ Pods/BuildHeaders/LRResty/NSRunLoop+Additions.h
@@ -1,16 +1,1 @@
-//
-//  NSRunLoop+Additions.h
-//  LRResty
-//
-//  Created by Luke Redpath on 20/01/2011.
-//  Copyright 2011 LJR Software Limited. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-
-@interface NSRunLoop (Additions)
-
-- (void)runForTimeInterval:(NSTimeInterval)interval;
-
-@end
+../../LRResty/Classes/Categories/NSRunLoop+Additions.h
