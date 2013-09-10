cf3d1332217f4181d04a4648db7b24f481796d7e
farthen <monkey@farthen.de>
2013-09-10 08:14:50 +0200

    FAContentTableViewCell: fix lots of layout bugs

--- Zapr/View/FAContentTableViewCell.m
+++ Zapr/View/FAContentTableViewCell.m
@@ -9,9 +9,7 @@
 #import "FAContentTableViewCell.h"
 #import "FATrakt.h"
 
-@implementation FAContentTableViewCell {
-    BOOL _addedConstraints;
-}
+@implementation FAContentTableViewCell
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
@@ -36,12 +34,15 @@
         } else if (![genres isEqualToString:@""]) {
             detailString = [NSString stringWithFormat:NSLocalizedString(@"%@", nil), genres];
         } else {
-            detailString = NSLocalizedString(@"", nil);
+            detailString = nil;
         }
         
         self.leftAuxiliaryTextLabel.text = detailString;
-        NSString *tagline = movie.tagline;
-        self.detailTextLabel.text = tagline;
+        if (movie.tagline && ![movie.tagline isEqual:[NSNull null]]) {
+            self.detailTextLabel.text = movie.tagline;
+        } else {
+            self.detailTextLabel.text = nil;
+        }
     } else if ([content isKindOfClass:[FATraktShow class]]) {
         // TODO: Crashbug here?
         FATraktShow *show = (FATraktShow *)content;
@@ -60,10 +61,15 @@
         } else if (![genres isEqualToString:@""]) {
             detailString = [NSString stringWithFormat:NSLocalizedString(@"%@", nil), genres];
         } else {
-            detailString = NSLocalizedString(@"", nil);
+            detailString = nil;
         }
         self.leftAuxiliaryTextLabel.text = detailString;
-        self.detailTextLabel.text = show.overview;
+        
+        if (show.overview && ![show.overview isEqual:[NSNull class]]) {
+            self.detailTextLabel.text = show.overview;
+        } else {
+            self.detailTextLabel.text = nil;
+        }
     } else if ([content isKindOfClass:[FATraktEpisode class]]) {
         FATraktEpisode *episode = (FATraktEpisode *)content;
         self.textLabel.text = episode.title;
@@ -75,11 +81,13 @@
         } else if (episode.overview) {
             self.detailTextLabel.text = [NSString stringWithFormat:NSLocalizedString(@"%@", nil), episode.overview];
         } else {
-            self.detailTextLabel.text = [NSString stringWithFormat:NSLocalizedString(@"", nil)];
+            self.detailTextLabel.text = nil;
         }
     } else {
         DDLogError(@"Tried to display a datatype in FASearchResultTableViewCell that is not possible to be displayed!");
     }
+    
+    [self setNeedsLayout];
 }
 
 - (void)layoutSubviews
@@ -92,8 +100,8 @@
     self.textLabel.textColor = [UIColor blackColor];
     
     UIFont *auxiliaryFont = [UIFont systemFontOfSize:14];
-    
     UIColor *auxiliaryTextColor = [UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:1.0];
+
     self.detailTextLabel.font = auxiliaryFont;
     self.detailTextLabel.textColor = auxiliaryTextColor;
     self.detailTextLabel.textAlignment = NSTextAlignmentLeft;
@@ -107,33 +115,36 @@
     self.leftAuxiliaryTextLabel.font = auxiliaryFont;
     self.leftAuxiliaryTextLabel.textColor = auxiliaryTextColor;
     self.leftAuxiliaryTextLabel.highlightedTextColor = [UIColor whiteColor];
-    self.detailTextLabel.textAlignment = NSTextAlignmentLeft;
     self.leftAuxiliaryTextLabel.lineBreakMode = NSLineBreakByTruncatingTail;
     
-    if (!_addedConstraints) {
-        // Create constraints for the title label:
-        self.textLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.textLabel setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
-        [self.textLabel setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisVertical];
-        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.textLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeTop multiplier:1 constant:5]];
-        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.textLabel attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeLeft multiplier:1 constant:widthMargin]];
-        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.textLabel attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationLessThanOrEqual toItem:self.contentView attribute:NSLayoutAttributeTrailing multiplier:1 constant:widthMargin]];
-        
-        // auxiliary label
-        self.leftAuxiliaryTextLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.textLabel attribute:NSLayoutAttributeBottom multiplier:1 constant:2]];
-        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.textLabel attribute:NSLayoutAttributeLeading multiplier:1 constant:0]];
-        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.textLabel attribute:NSLayoutAttributeTrailing multiplier:1 constant:0]];
-        
-        // Detail label
-        self.detailTextLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.detailTextLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeBottom multiplier:1 constant:2]];
-        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.detailTextLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeLeading multiplier:1 constant:0]];
-        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.detailTextLabel attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeTrailing multiplier:1 constant:0]];
-        
-        _addedConstraints = YES;
-    }
+    // Create constraints for the title label:
+    self.textLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.textLabel setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+    [self.textLabel setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisVertical];
+    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.textLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeTop multiplier:1 constant:5]];
+    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.textLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeLeading multiplier:1 constant:widthMargin]];
+    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.textLabel attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationLessThanOrEqual toItem:self.contentView attribute:NSLayoutAttributeTrailing multiplier:1 constant:widthMargin]];
     
+    // auxiliary label
+    self.leftAuxiliaryTextLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.leftAuxiliaryTextLabel setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+    [self.leftAuxiliaryTextLabel setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisVertical];
+    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.textLabel attribute:NSLayoutAttributeBottom multiplier:1 constant:2]];
+    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeLeading multiplier:1 constant:widthMargin]];
+    [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeTrailing multiplier:1 constant:widthMargin]];
+    
+    // Detail label
+    self.detailTextLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.detailTextLabel setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+    [self.detailTextLabel setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisVertical];
+    if ([self.detailTextLabel isDescendantOfView:self.contentView]) {
+        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.detailTextLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.leftAuxiliaryTextLabel attribute:NSLayoutAttributeBottom multiplier:1 constant:2]];
+        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.detailTextLabel attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeLeading multiplier:1 constant:widthMargin]];
+        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.detailTextLabel attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeTrailing multiplier:1 constant:widthMargin]];
+    } // watwatwat abandon ship (it's sad but it actually works)
+    // More thorough explanation: The detail text label seems to be removed when the text is nil. Why this is happening is pretty unclear. This fixes the segfault though ^^
+    
+    [self.contentView setNeedsUpdateConstraints];
     [self.contentView setNeedsLayout];
 }
 
