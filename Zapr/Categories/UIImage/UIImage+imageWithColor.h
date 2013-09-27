0473aa93d7cbf55fe457a5d7f5d8a13ffea5fe73
farthen <monkey@farthen.de>
2013-09-27 02:09:26 +0200

    Add some more categories that will shoot me in the foot later

---
+++ Zapr/Categories/UIImage/UIImage+imageWithColor.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+imageWithColor.h
+//  Zapr
+//
+//  Created by Finn Wilke on 27.09.13.
+//  Copyright (c) 2013 Finn Wilke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (imageWithColor)
+
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
+
+@end
---
+++ Zapr/Categories/UIImage/UIImage+imageWithColor.m
@@ -0,0 +1,28 @@
+//
+//  UIImage+imageWithColor.m
+//  Zapr
+//
+//  Created by Finn Wilke on 27.09.13.
+//  Copyright (c) 2013 Finn Wilke. All rights reserved.
+//
+
+#import "UIImage+imageWithColor.h"
+
+@implementation UIImage (imageWithColor)
+
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size
+{
+    CGRect rect = CGRectMake(0, 0, size.width, size.height);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
+@end
