cb43bed068e3f46aef06b37343a1b8ea30b4c526
farthen <monkey@farthen.de>
2013-10-16 22:24:51 +0200

    Fixes #29

--- Zapr/Controller/ViewController/FAContentBookmarkViewController.m
+++ Zapr/Controller/ViewController/FAContentBookmarkViewController.m
@@ -69,9 +69,10 @@
     self.displaysSeenButton = YES;
     if (content.contentType == FATraktContentTypeShows) {
         FATraktShow *show = (FATraktShow *)content;
-        if (show.progress.left.unsignedIntegerValue == 0) {
+        if (!show.progress ||
+            show.progress.left.unsignedIntegerValue == 0) {
             self.displaysSeenButton = NO;
-        }
+        }        
     }
     
     [self.tableView reloadData];
