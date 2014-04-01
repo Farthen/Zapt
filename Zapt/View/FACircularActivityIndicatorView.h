3cbac2df26a969fec7e7f236cdeeb3d28cfd95a8
farthen <monkey@farthen.de>
2014-04-01 22:08:41 +0200

    Add template for FACircularActivityIndicatorView. May never be finished but at least it’s a good reminder!

---
+++ Zapt/View/FACircularActivityIndicatorView.h
@@ -0,0 +1,13 @@
+//
+//  FACircularActivityIndicatorView.h
+//  Zapt
+//
+//  Created by Finn Wilke on 09/03/14.
+//  Copyright (c) 2014 Finn Wilke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FACircularActivityIndicatorView : UIView
+
+@end
