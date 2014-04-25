82ec756c580c4dcf2f872f7dec891692bc29f111
farthen <monkey@farthen.de>
2014-04-25 21:59:16 +0200

    Some cleanup, make acknowledgements nicer

---
+++ Zapt/Controller/ViewController/Main Interface/Settings/FAAcknowledgementsGenerator.h
@@ -0,0 +1,13 @@
+//
+//  FAAcknowledgementsGenerator.h
+//  Zapt
+//
+//  Created by Finn Wilke on 25/04/14.
+//  Copyright (c) 2014 Finn Wilke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FAAcknowledgementsGenerator : NSObject
+
+@end
---
+++ Zapt/Controller/ViewController/Main Interface/Settings/FAAcknowledgementsGenerator.m
@@ -0,0 +1,13 @@
+//
+//  FAAcknowledgementsGenerator.m
+//  Zapt
+//
+//  Created by Finn Wilke on 25/04/14.
+//  Copyright (c) 2014 Finn Wilke. All rights reserved.
+//
+
+#import "FAAcknowledgementsGenerator.h"
+
+@implementation FAAcknowledgementsGenerator
+
+@end
