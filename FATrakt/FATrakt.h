5da8cc76f240e477c89ec59670e00d3b49d27520
farthen <monkey@farthen.de>
2014-06-09 19:28:56 +0200

    Add missing files

--- FATrakt/FATrakt.h
+++ FATrakt/FATrakt.h
@@ -16,4 +16,9 @@ FOUNDATION_EXPORT const unsigned char FATraktVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FATrakt/PublicHeader.h>
 
+#ifndef __FA_TRAKT_H
+#define __FA_TRAKT_H
 
+#import "FATraktAPI.h"
+
+#endif //__FA_TRAKT_H
